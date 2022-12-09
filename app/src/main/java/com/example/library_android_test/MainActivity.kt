feaffdafe7a135f14e3d5a8563e2f13c2deb4235
nghia cau <caonghia97@gmail.com>
2022-12-09 15:40:02 +0700

    update tag 2.0.0

--- app/src/main/java/com/example/library_android_test/MainActivity.kt
+++ app/src/main/java/com/example/library_android_test/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    private fun initViews(){
+        val color = getColor(com.example.librarycolor.R.color.com_3nq_color_amber_300)
     }
 }
